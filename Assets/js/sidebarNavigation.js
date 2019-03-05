9f1f2c4242c16d3c2be09ab163343bc899a79461
xuanh251 <lexuanhoang1997@gmail.com>
2019-03-05 13:54:05 +0700

    optimizecode-ud1

--- Assets/js/sidebarNavigation.js
+++ Assets/js/sidebarNavigation.js
@@ -1,21 +1,3 @@
 $(document).ready(function(){
-    // var fixHeight = function(){
-    //   $(".navbar-nav").css("max-height",document.documentElement.clientHeight -150);
-    // }
-    // fixHeight();
-    // $(window).resize(function(){
-    //  fixHeight();
-    // });
-    // $(".navbar .navbar-toggler").on("click",function(){
-    //   fixHeight();
-    // });
-    $(".btn-nav-onleft").on("click",function(){
-      $(".mobileMenuLeft").toggleClass("open");
-      $('.mobileMenuLeft').toggleClass("fixHeight");
-    });
-    $(".btn-nav-onright").on("click",function(){
-      $(".mobileMenuRight").toggleClass("open");
-      $('.mobileMenuRight').toggleClass("initHeight");
-      $('.mobileMenuRight').toggleClass("undisplay");
-    });
+   
   });
--- Assets/js/udn.js
+++ Assets/js/udn.js
@@ -4,15 +4,30 @@ var udn = {
     udn.checkResize();
   },
   registerEvent: function () {
+    //show-hide left child menu
     $('.udn-sidebar-content li ul').hide();
     $(".udn-sidebar-content a.toggle").off('click').on('click', function () {
       $(this).next("ul").slideToggle();
       $(this).closest("li").toggleClass("li-active");
       $('a.toggle span').toggleClass('udn-childdropdown-active');
     });
+
+    //swipe menu from left and right
+    $(".btn-nav-onleft").on("click",function(){
+      $(".mobileMenuLeft").toggleClass("open");
+      $('.mobileMenuLeft').toggleClass("fixHeight");
+    });
+    $(".btn-nav-onright").on("click",function(){
+      $(".mobileMenuRight").toggleClass("open");
+      $('.mobileMenuRight').toggleClass("initHeight");
+    });
+
+
     $(window).resize(function () {
       udn.checkResize();
     });
+
+    //append data to select
     Date.prototype.getWeek = function () {
       var a = new Date(this.getFullYear(), 0, 1);
       return Math.ceil((((this - a) / 86400000) + a.getDay() + 1) / 7);
@@ -25,6 +40,18 @@ var udn = {
     }));
     $('#fromDaytoDay').text("Từ " + udn.formatDate(udn.getMonday(new Date())) + " đến " + udn.getSunday(udn.getMonday(new Date())));
   },
+
+  checkResize: function () {
+    if ($(window).width() <= 991) {
+      $('#first-search-form').hide();
+      $('#second-search-form').show();
+    } else {
+      $('#first-search-form').show();
+      $('#second-search-form').hide();
+    }
+  },
+
+  //get special day
   getMonday: function (date) {
     var day = date.getDay() || 7;
     if (day !== 1)
@@ -50,26 +77,7 @@ var udn = {
     if (day.length < 2) day = '0' + day;
 
     return [day, month, year].join('-');
-  },
-  checkResize: function () {
-    if ($(window).width() <= 991) {
-      $('#first-search-form').hide();
-      $('#second-search-form').show();
-      // var listClassname=$('#collapseOne').attr('class');
-      // var show ='show';
-      // if(listClassname.indexOf(show)!=-1){
-      //   $('#collapseOne').removeClass('show');
-      // }
-    } else {
-      $('#first-search-form').show();
-      $('#second-search-form').hide();
-      // $('#collapseOne').addClass('show');
-    }
-    // if ($(window).width() <= 545) {
-    //   $('#top-link ul li:not(.li-img)').hide();
-    // }else{
-    //   $('#top-link ul li:not(.li-img)').show();
-    // }
   }
+
 }
 udn.init();
